d4def5c9ccda4ba217fada2898a753c04d6675f0
Kamlesh Panchal <kpanchal@redhat.com>
2024-06-21 16:39:40 -0400

    Use flyway image in SFP deployment

--- flyway-db-migration-docker-image/db-migration-files/B1.0.0__di_create_schema.sql
+++ flyway-db-migration-docker-image/db-migration-files/B1.0.0__di_create_schema.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA IF EXISTS "di-db";
-CREATE SCHEMA "di-db";
-DROP SCHEMA IF EXISTS "js-db";
-CREATE SCHEMA "js-db";
+DROP SCHEMA IF EXISTS "data-index-service";
+CREATE SCHEMA "data-index-service";
+DROP SCHEMA IF EXISTS "jobs-service";
+CREATE SCHEMA "jobs-service";
