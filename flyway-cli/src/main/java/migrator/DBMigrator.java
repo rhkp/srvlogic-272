8b07f4ca9f134409f7013d78e81c5dfa2ce6a763
rhkp <kpanchal@redhat.com>
2024-08-01 16:16:01 -0400

    Add exception handling

--- flyway-cli/src/main/java/migrator/DBMigrator.java
+++ flyway-cli/src/main/java/migrator/DBMigrator.java
@@ -3,6 +3,7 @@ package migrator;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
+import io.quarkus.logging.Log; 
 
 @QuarkusMain
 public class DBMigrator implements QuarkusApplication {
@@ -11,9 +12,15 @@ public class DBMigrator implements QuarkusApplication {
     MigrationService service;
 
     @Override
-    public int run(String... args) throws Exception {
-        service.checkMigration();
-        System.exit(0);
-        return 0;
+    public int run(String... args) {
+        try {
+            service.checkMigration();
+            System.exit(0);
+            return 0;
+        } catch(Exception e) {
+            Log.error( "Error performing database migration: " + e.toString());
+        }
+        System.exit(-1);
+        return -1;
     }
 }
